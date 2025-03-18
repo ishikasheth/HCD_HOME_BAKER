f597e781fd06813bff6e98a68f90099f55545c9a
Priya Shukla <priyu184@gmail.com>
2025-03-18 13:15:14 +0530

    Changes

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import './Home.css'
+import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 
 const Home = () => {
 
@@ -8,7 +10,8 @@ const Home = () => {
 
   return (
     <div>
-      
+      <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
     </div>
   )
 }
