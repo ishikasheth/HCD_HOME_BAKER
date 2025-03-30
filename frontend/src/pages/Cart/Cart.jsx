ebbad9cd93639e4b994b195116846c670abeba53
Priya Shukla <priyu184@gmail.com>
2025-03-30 11:39:33 +0530

    Cart page - PlaceOrder

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -68,7 +68,7 @@ const Cart = () => {
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>₹{getTotalCartAmount()===0 ? 0 : getTotalCartAmount() + 2}</b>
+              <b>₹{getTotalCartAmount()===0 ? 0 : getTotalCartAmount() + 200}</b>
             </div>            
           </div>
           <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -43,7 +43,7 @@ const PlaceOrder = () => {
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>₹{getTotalCartAmount()===0 ? 0 : getTotalCartAmount() + 2}</b>
+              <b>₹{getTotalCartAmount()===0 ? 0 : getTotalCartAmount() + 200}</b>
             </div> 
           </div>
           <button>PROCEED TO Payment</button>
